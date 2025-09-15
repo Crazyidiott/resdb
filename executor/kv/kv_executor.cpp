3a54f471470d9266e46f93dc218cb3426eb5a410
Crazyidiott <1073405983@qq.com>
2025-09-15 20:19:52 +0800

    worked pipeline: manual configuration

--- executor/kv/kv_executor.cpp
+++ executor/kv/kv_executor.cpp
@@ -19,7 +19,6 @@
 
 #include "executor/kv/kv_executor.h"
 #include "executor/contract/executor/contract_executor.h"
-// #include <openssl/sha.h>
 #include <glog/logging.h>
 
 namespace resdb {
@@ -45,19 +44,6 @@ std::unique_ptr<std::string> KVExecutor::ExecuteRequest(
   const KVRequest& kv_request = dynamic_cast<const KVRequest&>(request);
 
   if (kv_request.cmd() == KVRequest::SET) {
-    // std::string input = "Hello, World!";
-    // std::vector<unsigned char> hash(SHA256_DIGEST_LENGTH);
-    
-    // for (int i = 0; i < 10000; i++) {
-    //     SHA256(reinterpret_cast<const unsigned char*>(input.data()),
-    //            input.size(),
-    //            hash.data());
-        
-    //     // 将当前哈希结果作为下一次的输入
-    //     input.assign(hash.begin(), hash.end());
-    // }
-    LOG(ERROR)<<" execute cmd:";
-
     Set(kv_request.key(), kv_request.value());
   } else if (kv_request.cmd() == KVRequest::GET) {
     kv_response.set_value(Get(kv_request.key()));
@@ -108,18 +94,6 @@ std::unique_ptr<std::string> KVExecutor::ExecuteData(
   }
 
   if (kv_request.cmd() == KVRequest::SET) {    
-    // std::string input = "Hello, World!";
-    // std::vector<unsigned char> hash(SHA256_DIGEST_LENGTH);
-    
-    // for (int i = 0; i < 10000; i++) {
-    //     SHA256(reinterpret_cast<const unsigned char*>(input.data()),
-    //            input.size(),
-    //            hash.data());
-        
-    //     // 将当前哈希结果作为下一次的输入
-    //     input.assign(hash.begin(), hash.end());
-    // }
-    LOG(ERROR)<<" execute cmd:";
     Set(kv_request.key(), kv_request.value());
   } else if (kv_request.cmd() == KVRequest::GET) {
     kv_response.set_value(Get(kv_request.key()));
